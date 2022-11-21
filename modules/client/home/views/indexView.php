37082dabe0e8b3f882fc8b4d1fc50a3048b4d7cf
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-11-22 00:17:06 +0700

    up

--- modules/client/home/views/indexView.php
+++ modules/client/home/views/indexView.php
@@ -95,7 +95,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md ">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/26.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -105,7 +105,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md ">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/27.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -115,7 +115,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md ">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/28.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -129,7 +129,7 @@
 
         <div class="flex justify-between mt-4 ml-2">
             <div class="border-2 mr-4  rounded-md">
-                <img src="assets/media/products/19.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/29.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -139,7 +139,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/30.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -149,7 +149,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md ">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/1.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -159,7 +159,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md ">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/2.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
@@ -169,7 +169,7 @@
             </div>
 
             <div class="border-2  mr-4  rounded-md ">
-                <img src="assets/media/products/20.png" alt="" class="w-[100px]  h-[280px] m-auto">
+                <img src="assets/media/products/3.png" alt="" class="w-[100px]  h-[280px] m-auto">
 
                 <p class="text-xl text-slate-500 pb-4 pl-2 text-center ">Siêu tinh chất Vita-C Total Care Ampule
                     Newland...</p>
