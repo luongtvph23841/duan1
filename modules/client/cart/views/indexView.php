019351ca5fe55f73aa807806fa66cdec3d6e96b7
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-11-25 14:19:59 +0700

    up

--- modules/client/cart/views/indexView.php
+++ modules/client/cart/views/indexView.php
@@ -25,19 +25,17 @@
 
         <table class="w-[100%]">
             <tr class="bg-gray-200">
-                <th class="border-2 border-black ">Mã sản phẩm</th>
+                <th class="border-2 border-black ">Tên sản phẩm</th>
                 <th class="border-2 border-black ">Hình ảnh sản phẩm</th>
                 <th class="border-2 border-black ">Thương hiệu</th>
-                <th class="border-2 border-black ">SiZe</th>
                 <th class="border-2 border-black ">Số Lượng</th>
                 <th class="border-2 border-black ">Thành Tiền</th>
                 <th class="border-2 border-black ">Thao tác</th>
             </tr>
             <tr>
-                <td class="border-2 border-black text-center  ">PH24073</td>
+                <td class="border-2 border-black text-center ">PH24073</td>
                 <td class="border-2 border-black text-center w-[200px] h-[200px]"><img src="img/aa1.jpg" alt=""></td>
                 <td class="border-2 border-black text-center ">ADIDAS</td>
-                <td class="border-2 border-black text-center  "> M</td>
                 <td class="border-2 border-black text-center ">2</td>
                 <td class="border-2 border-black text-center text-red-500 ">100.000</td>
                 <td class="border-2 border-black text-center "><i class="fa fa-trash"></i></td>
