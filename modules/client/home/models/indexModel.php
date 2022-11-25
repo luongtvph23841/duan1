deea81f14c623fd1fb6798a5533be063523afaa1
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-11-25 16:23:20 +0700

    up-client-login

--- modules/client/home/models/indexModel.php
+++ modules/client/home/models/indexModel.php
@@ -5,3 +5,9 @@ function get_list_products()
     $result = db_fetch_array("SELECT * FROM `products`");
     return $result;
 }
+
+function get_list_users()
+{
+    $result = db_fetch_array("SELECT * FROM `users`");
+    return $result;
+}
