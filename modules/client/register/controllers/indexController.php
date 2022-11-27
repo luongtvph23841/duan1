3422d90802c1bafc9fff71ffe8632c5c836c638a
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-11-28 00:24:53 +0700

    up-client-register

--- modules/client/register/controllers/indexController.php
+++ modules/client/register/controllers/indexController.php
@@ -1,9 +1,28 @@
 <?php
 
-function construct() {
+function construct()
+{
     load_model('index');
 }
 
-function indexAction() {
+function indexAction()
+{
     load_view('index');
+}
+
+function createAction()
+{
+    load_view('create');
+}
+
+function indexPostAction()
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+
+    create_user($name, $email, $password, $phone, $address);
+    header('Location: ?role=client&mod=login');
 }
