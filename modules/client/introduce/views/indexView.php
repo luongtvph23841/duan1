f8aa91547c293f3da3e61bd432c46b9370074ee3
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-11-28 23:07:08 +0700

    up

--- modules/client/introduce/views/indexView.php
+++ modules/client/introduce/views/indexView.php
@@ -1,71 +1,94 @@
 <?php get_header('', 'Giới thiệu') ?>
-<div class="mx-auto container">
-        
-        <div>
-        <p>
-           <h2 class="text-[30px]"> BEAUSKIN - Nguồn nguyên liệu khỏe mang đến vẻ đẹp tự nhiên </h2>
-            Mỹ phẩm Beauskin là thương hiệu mỹ phẩm thiên nhiên thuần khiết của Hàn quốc ra đời năm 2005 . Với slogan “nguyên liệu khỏe làn da đẹp”. Beauskin đại diện cho nỗ lực chân thành hướng đến mục tiêu chăm sóc làn da từ sâu bên trong, giúp phụ nữ tự tin và đầy quyến rũ. 
 
-            Trên hành trình đi tìm nguồn nguyên liệu để tạo nên những dòng sản phẩm chăm sóc da, chúng tôi luôn đặt ra tiêu chí: “Nguồn nguyên liệu khỏe – Chăm sóc toàn diện cho làn da”. 
-
-            Không chỉ là những loại thảo mộc có nguồn gốc hoàn toàn thiên nhiên mà chúng còn được gieo trồng và chăm sóc trong điều kiện đặc biệt ở những vùng có thổ nhưỡng tốt nhất, không khí trong lành nhất. 
-
+<div class="mx-auto w-[80%]">
+    <div class="text-center mb-[50px] border-b-[1px]">
+        <p class="uppercase text-[36px] font-bold text-[#333333] capitalize p-[30px] ">giới thiệu</p>
+    </div>
+    <div>
+        <h2 class="text-[36px] font-bold text-[#333333] capitalize mb-[15px]"> BEAUSKIN - Nguồn nguyên liệu khỏe mang đến vẻ đẹp tự nhiên </h2>
+        <p class="mb-[15px]">
+            Mỹ phẩm Beauskin là thương hiệu mỹ phẩm thiên nhiên thuần khiết của Hàn quốc ra đời năm 2005 . Với slogan “nguyên liệu khỏe làn da đẹp”. Beauskin đại diện cho nỗ lực chân thành hướng đến mục tiêu chăm sóc làn da từ sâu bên trong, giúp phụ nữ tự tin và đầy quyến rũ.
+        </p>
+        <p class="mb-[15px]">
+            Trên hành trình đi tìm nguồn nguyên liệu để tạo nên những dòng sản phẩm chăm sóc da, chúng tôi luôn đặt ra tiêu chí: “Nguồn nguyên liệu khỏe – Chăm sóc toàn diện cho làn da”.
+        </p>
+        <p class="mb-[15px]">
+            Không chỉ là những loại thảo mộc có nguồn gốc hoàn toàn thiên nhiên mà chúng còn được gieo trồng và chăm sóc trong điều kiện đặc biệt ở những vùng có thổ nhưỡng tốt nhất, không khí trong lành nhất.
+        </p>
+        <p class="mb-[15px]">
             Để sở hữu hàm lượng và dưỡng chất có trong mỗi loài thảo mộc được sản sinh một cách dồi dào nhất. Beauskin đã quyết định lựa chọn Gyeongsan, Geumsan và Jeju làm điểm cung cấp nguồn nguyên liệu chính cho quá trình sản xuất.
         </p>
-        </div>
-        <div>
-        <p> 
-            <h2 class="text-[30px]"> Mỹ phẩm Beauskin và các vùng thổ nhưỡng tốt nhất tại Hàn </h2>
-            Nhằm mục đích chia sẻ lợi ích từ thiên nhiên tinh khiết, hướng tới vẻ đẹp khỏe mạnh thực sự, chúng tôi đã chọn những vùng có không khí trong lành, ánh nắng dịu nhẹ, đất đai màu mỡ, nguồn nước sạch và không bị ô nhiễm. 
-            
-            Từ khâu gieo trồng đến quy trình chăm bón và thu hoạch tất cả đều được chăm chút bởi nguồn năng lượng thiên nhiên quý báu. 
-            
-            Khởi đầu từ nguồn nguyên liệu tự nhiên Beauskin đã khám phá và lựa chọn ra được những loại thảo dược có hàm lượng dưỡng chất tốt nhất, đặc trưng của từng vùng để tạo nên bí quyết làm đẹp riêng cho thương hiệu của mình. 
+    </div>
+    <div>
+        <h2 class="text-[36px] font-bold text-[#333333] capitalize mb-[15px]"> Mỹ phẩm Beauskin và các vùng thổ nhưỡng tốt nhất tại Hàn </h2>
+        <p class="mb-[15px]">
+            Nhằm mục đích chia sẻ lợi ích từ thiên nhiên tinh khiết, hướng tới vẻ đẹp khỏe mạnh thực sự, chúng tôi đã chọn những vùng có không khí trong lành, ánh nắng dịu nhẹ, đất đai màu mỡ, nguồn nước sạch và không bị ô nhiễm.
         </p>
-        </div>
-        <div>
-        <p>
-            <h2 class="text-[30px]"> Nguồn gốc các nguyên liệu của mỹ phẩm Beauskin </h2>
-            <h2 class="text-[25px]"> Rau má hữu cơ </h2>
-            Rau má hữu cơ phát triển tốt nhờ vùng thổ nhưỡng tốt nhất trên đảo Jeju.
-            Với năng lượng tự nhiên cùng nguồn dinh dưỡng từ đất trồng nên chúng có thể phát huy tối đa hiệu quả. 
-            Beauskin đã sử dụng rau má hữu cơ được chăm sóc tỉ mỉ này để tạo ra dòng skincare rau má giúp tái tạo, phục hồi làn da tổn thương. 
-            <h2 class="text-[25px]"> Táo đỏ vùng Gyeongsan </h2>
-            Trong táo còn chứa AHA tự nhiên, hoạt chất catechin giúp loại bỏ đi những tế bào chết xỉn màu một cách nhẹ nhàng nhất, thanh lọc làn da hiệu quả để đem đến làn da sáng mịn, sạch khỏe và căng mọng hơn.  Beauskin đã lựa chọn kỹ càng giống táo trồng ở nơi được mệnh danh là “Thung lũng táo đỏ” – Gyeongsan
-
-            Cùng các nguồn nguyên liệu khỏe khác như nha đam, trà xanh, olive, tinh than tre…đáp ứng điều kiện quy trình chăm sóc nghiêm ngặt để tạo ra những dưỡng chất tinh túy nhất.
-            <h2 class="text-[25px]">
-                Công thức ngăn ngừa lão hóa
-            </h2>
-            Những nguồn nguyên liệu thảo mộc tự nhiên quý hiếm rất giàu dưỡng chất khỏe mạnh đối với làn da thế nhưng, tính ổn định của các dưỡng chất này lại không đảm bảo. Beauskin đã kết hợp những công nghệ mới cùng công thức ngăn ngừa lão hóa độc quyền chỉ giữ lại các 
-            thành tố dưỡng chất khỏe và tinh túy nhất giúp tăng cường khả năng hấp thụ trên da. Đẩy nhanh hiệu quả phục hồi và tái tạo các tế bào da mới.
-            <h2 class="text-[25px]">Loại bỏ hoàn toàn các thành phần gây hại cho da</h2>
-            Beauskin luôn nỗ lực trong việc tạo ra các dòng sản phẩm chăm sóc da với 100% thành phần thiên nhiên, không chứa các thành phần gây hại như:
-             Alcohol, paraben, mineral, bột Talc,  TEA, hương liệu, phẩm màu…để ngay cả những làn da nhạy cảm nhất cũng được chăm sóc và bảo vệ.
-            
+        <p class="mb-[15px]">
+            Từ khâu gieo trồng đến quy trình chăm bón và thu hoạch tất cả đều được chăm chút bởi nguồn năng lượng thiên nhiên quý báu.
         </p>
-        </div>
-        <div>
-        <p>
-            <h2 class="text-[30px]"> Ý nghĩa tên thương hiệu BEAUSKIN</h2>
-            Logo của chúng tôi đại diện cho nỗ lực chân thành của Beauskin để mang đến vẻ đẹp khỏe mạnh và là tên viết tắt của cụm từ “Beautiful Skin”.
-            <h2 class="text-[20px]">B - BEAUTY: Vẻ đẹp</h2>
-            Beauskin vạch ra hướng đi riêng cho mình trên hành trình đem đến vẻ đẹp tự nhiên hoàn hảo cho người phụ nữ với phương châm: Chăm sóc làn da rạng rỡ bên ngoài và khỏe mạnh bên trong.
-            <h2 class="text-[20px]">E - Eco friendly: Nguyên liệu khỏe, tinh túy từ thiên nhiên</h2>
-            Nguồn nguyên liệu sạch, khỏe và thân thiện với môi trường được sàng lọc từ những vùng núi cao, nơi có thổ nhưỡng và khí hậu trong lành nhất tại Hàn Quốc như: Gyeongsan, Gumsan, Jeju…
-            <h2 class="text-[20px]">A - Attractive or Alluring: Làn da khỏe đẹp và quyến rũ</h2>
-            Beauskin hướng đến những dòng sản phẩm có tính năng chăm sóc, bảo vệ và giữ gìn sự khỏe mạnh kể cả làn da nhạy cảm. 
-            <h2 class="text-[20px]">U - Unadorned: Làn da đẹp tinh khiết </h2>
-            Kết hợp các nguồn nguyên liệu khỏe cùng công nghệ mới nhất trong ngành dưỡng da với những bí quyết riêng nhằm cân bằng tính ổn định về kết cấu sản phẩm. Hỗ trợ tăng cường khả năng hấp thu và thẩm thấu tận sâu bên trong da. Mang đến một làn da đẹp rạng rỡ, thuần khiết không cần trang điểm.  
-
-            Beauskin - Tất cả vì sự khỏe mạnh và vẻ đẹp tự nhiên của làn da!
-         </p>
-        </div>
-       
-        <div class="flex justify-center">
-            <div>
-        <img src="/img/1.jpg" alt="">
-            </div>
+        <p class="mb-[15px]">
+            Khởi đầu từ nguồn nguyên liệu tự nhiên Beauskin đã khám phá và lựa chọn ra được những loại thảo dược có hàm lượng dưỡng chất tốt nhất, đặc trưng của từng vùng để tạo nên bí quyết làm đẹp riêng cho thương hiệu của mình.
+        </p>
+    </div>
+    <div>
+        <h2 class="text-[36px] font-bold text-[#333333] capitalize mb-[15px]"> Nguồn gốc các nguyên liệu của mỹ phẩm Beauskin </h2>
+        <div class="ml-5">
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]"> Rau má hữu cơ </h3>
+            <p class="mb-[15px]">
+                Rau má hữu cơ phát triển tốt nhờ vùng thổ nhưỡng tốt nhất trên đảo Jeju.
+                Với năng lượng tự nhiên cùng nguồn dinh dưỡng từ đất trồng nên chúng có thể phát huy tối đa hiệu quả.
+                Beauskin đã sử dụng rau má hữu cơ được chăm sóc tỉ mỉ này để tạo ra dòng skincare rau má giúp tái tạo, phục hồi làn da tổn thương.
+            </p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">Táo đỏ vùng Gyeongsan</h3>
+            <p class="mb-[15px]">
+                Trong táo còn chứa AHA tự nhiên, hoạt chất catechin giúp loại bỏ đi những tế bào chết xỉn màu một cách nhẹ nhàng nhất, thanh lọc làn da hiệu quả để đem đến làn da sáng mịn, sạch khỏe và căng mọng hơn. Beauskin đã lựa chọn kỹ càng giống táo trồng ở nơi được mệnh danh là “Thung lũng táo đỏ” – Gyeongsan
+            </p>
+            <p class="mb-[15px]">
+                Cùng các nguồn nguyên liệu khỏe khác như nha đam, trà xanh, olive, tinh than tre…đáp ứng điều kiện quy trình chăm sóc nghiêm ngặt để tạo ra những dưỡng chất tinh túy nhất.
+            </p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">Công thức ngăn ngừa lão hóa</h3>
+            <p class="mb-[15px]">
+                Những nguồn nguyên liệu thảo mộc tự nhiên quý hiếm rất giàu dưỡng chất khỏe mạnh đối với làn da thế nhưng, tính ổn định của các dưỡng chất này lại không đảm bảo. Beauskin đã kết hợp những công nghệ mới cùng công thức ngăn ngừa lão hóa độc quyền chỉ giữ lại các
+                thành tố dưỡng chất khỏe và tinh túy nhất giúp tăng cường khả năng hấp thụ trên da. Đẩy nhanh hiệu quả phục hồi và tái tạo các tế bào da mới.</p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">Loại bỏ hoàn toàn các thành phần gây hại cho da</h3>
+            <p class="mb-[15px]">
+                Beauskin luôn nỗ lực trong việc tạo ra các dòng sản phẩm chăm sóc da với 100% thành phần thiên nhiên, không chứa các thành phần gây hại như:
+                Alcohol, paraben, mineral, bột Talc, TEA, hương liệu, phẩm màu…để ngay cả những làn da nhạy cảm nhất cũng được chăm sóc và bảo vệ.
+            </p>
         </div>
     </div>
+    <div>
+
+        <h2 class="text-[36px] font-bold text-[#333333] capitalize mb-[15px]"> Ý nghĩa tên thương hiệu BEAUSKIN</h2>
+        <div class="ml-5">
+            <p class="mb-[15px]">
+                Logo của chúng tôi đại diện cho nỗ lực chân thành của Beauskin để mang đến vẻ đẹp khỏe mạnh và là tên viết tắt của cụm từ “Beautiful Skin”.
+            </p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">B - BEAUTY: Vẻ đẹp</h3>
+            <p class="mb-[15px]">
+                Beauskin vạch ra hướng đi riêng cho mình trên hành trình đem đến vẻ đẹp tự nhiên hoàn hảo cho người phụ nữ với phương châm: Chăm sóc làn da rạng rỡ bên ngoài và khỏe mạnh bên trong.
+            </p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">E - Eco friendly: Nguyên liệu khỏe, tinh túy từ thiên nhiên</h3>
+            <p class="mb-[15px]">
+                Nguồn nguyên liệu sạch, khỏe và thân thiện với môi trường được sàng lọc từ những vùng núi cao, nơi có thổ nhưỡng và khí hậu trong lành nhất tại Hàn Quốc như: Gyeongsan, Gumsan, Jeju…
+            </p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">A - Attractive or Alluring: Làn da khỏe đẹp và quyến rũ</h3>
+            <p class="mb-[15px]">
+                Beauskin hướng đến những dòng sản phẩm có tính năng chăm sóc, bảo vệ và giữ gìn sự khỏe mạnh kể cả làn da nhạy cảm.
+            </p>
+            <h3 class="text-[25px] font-bold text-[#333333] capitalize mb-[15px]">U - Unadorned: Làn da đẹp tinh khiết </h3>
+            <p class="mb-[15px]">
+                Kết hợp các nguồn nguyên liệu khỏe cùng công nghệ mới nhất trong ngành dưỡng da với những bí quyết riêng nhằm cân bằng tính ổn định về kết cấu sản phẩm. Hỗ trợ tăng cường khả năng hấp thu và thẩm thấu tận sâu bên trong da. Mang đến một làn da đẹp rạng rỡ, thuần khiết không cần trang điểm.
+            </p>
+            <p class="mb-[15px]">
+                Beauskin - Tất cả vì sự khỏe mạnh và vẻ đẹp tự nhiên của làn da!</p>
+            </p>
+        </div>
+    </div>
+
+    <div class="flex justify-center mb-10 mt-10">
+        <img src="assets/media/project-logos/introduce.jpg" alt="">
+    </div>
+</div>
 <?php get_footer() ?>
