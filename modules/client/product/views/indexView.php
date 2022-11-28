0f33289a9f63a9685df54c6dfba3b14061b69386
vantiendeptrai <tienlvph24073@fpt.edu.vn>
2022-11-28 15:23:49 +0700

    tienlvph24073_cleant_layout_product

--- modules/client/product/views/indexView.php
+++ modules/client/product/views/indexView.php
@@ -1,35 +1,36 @@
 <?php get_header('', 'Sản phẩm') ?>
 
 <div class="w-full my-3">
-    <div class="grid grid-cols-4 gap-10 w-[90%] mx-auto">
-        <?php foreach ($products as $product) : ?>
-            <div class="mx-auto h-auto text-center">
-                <div class="">
-                    <div class="menu h-[250px] w-[270px] bg-[#f8f8f8] rounded-t-[30px] rounded-b-[30px]">
-                        <a href="?role=client&mod=productinfo&id_prod=<?php echo $product['id'] ?>">
-                            <img src="assets/media/products/<?php echo $product['image'] ?>" alt="" class="w-[200px] h-[210px] pt-2 mx-auto">
-                        </a>
-                        <div class="dropmenu absolute bg-[pink] w-[270px] leading-[45px] rounded-b-[30px]">
-                            <div class="flex justify-around mt-1">
-                                <a href="?role=client&mod=productinfo&id_prod=<?php echo $product['id'] ?>">
-                                    <i class="fa fa-eye text-[30px] hover:text-pink-100"></i>
-                                </a>
-                                <a href="?role=client&mod=cart">
-                                    <i class="fa fa-shopping-cart text-[30px] hover:text-pink-100"></i>
-                                </a>
+        <div class="grid grid-cols-4 gap-10 w-[90%] mx-auto">
+            <?php foreach ($products as $product) : ?>
+                <div class="mx-auto h-auto text-center">
+                    <div class="">
+                        <div class="menu h-[300px] w-[270px] bg-[#f8f8f8] rounded-t  hover:border">
+                            <a href="?role=client&mod=productinfo&id_prod=<?php echo $product['id'] ?>">
+                                <img src="assets/media/products/<?php echo $product['image'] ?>" alt="" class="w-[200px] h-[210px] pt-2 mx-auto ">
+                            </a>
+                            <div class="dropmenu absolute bg-stone-500 w-[270px] leading-[45px]  mt-10 ">
+                                <div class="flex justify-around mt-1">
+                                    <a href="?role=client&mod=cart">
+                                        <p class="text-white text-base pt-2 hover:text-amber-500"><i class="fa fa-plus" aria-hidden="true"></i> ADD TO CART</p>
+                                    </a>
+                                    <a href="?role=client&mod=productinfo&id_prod=<?php echo $product['id'] ?>">
+                                        <i class="fa fa-eye text-[30px] text-white hover:text-amber-500"></i>
+                                    </a>
+
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="mt-[30px]">
+                        <h3 class="color-black uppercase font-semibold leading-[16px]">
+                            <a href="?role=client&mod=productinfo&id_prod=<?php echo $product['id'] ?>" class="hover:text-pink-800"><?php echo $product['name_product'] ?></a>
+                        </h3>
+                        <h4 class="color-black leading-[16px] text-[14px] text-[red] mt-[15px]"><?php echo $product['price'] ?> đ</h4>
+                    </div>
                 </div>
-                <div class="mt-[30px]">
-                    <h3 class="color-black uppercase font-semibold leading-[16px]">
-                        <a href="?role=client&mod=productinfo&id_prod=<?php echo $product['id'] ?>" class="hover:text-pink-800"><?php echo $product['name_product'] ?></a>
-                    </h3>
-                    <h4 class="color-black leading-[16px] text-[14px] text-[red] mt-[15px]"><?php echo $product['price'] ?> đ</h4>
-                </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
-</div>
 
 <?php get_footer() ?>
--- modules/client/productinfo/views/indexView.php
+++ modules/client/productinfo/views/indexView.php
@@ -4,17 +4,17 @@
     <a href="?role=client&mod=product">Sản phẩm</a> <i class="fa fa-angle-right m-[5px]"> </i><?php echo $product['name_product'] ?>
 </div>
 
-<div class="flex font-serif w-[1000px] h-[500px] mt-[50px] mx-auto">
-    <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 pr-8   ">
-        <img src="assets/media/products/<?php echo $product['image'] ?>" alt="" class="h-[400px] h-[450px] border rounded " loading="lazy" />
+<div class="flex font-serif w-[1000px] h-[500px]  mt-[50px] mx-auto">
+    <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 pr-8 bg-[#f8f8f8] mr-6 rounded mb-4 ">
+        <img src="assets/media/products/<?php echo $product['image'] ?>" alt="" class="h-[400] w-full mt-20   " loading="lazy" />
     </div>
     <form class="col-lg-6 col-md-6 col-sm-12 col-xs-12 information-product w-full">
         <div class="flex flex-wrap items-baseline">
-            <h1 class="w-full flex-none mb-3 text-2xl leading-none text-slate-900 font-black text-slate-600 ">
+            <h1 class="w-full flex-none mb-3 text-2xl leading-none text-slate-900  text-slate-600 ">
                 <?php echo $product['name_product'] ?>
             </h1>
             <div class="flex-auto text-lg font-medium text-slate-500 h-[40px] border-b-2">
-                <?php echo $product['price'] ?> đ
+                <?php echo $product['price'] ?>.đ
             </div>
             <div class="text-xs leading-6 font-medium uppercase text-slate-500">
                 In stock
@@ -25,12 +25,12 @@
             <div class="box-quantity h-[50px]">
                 <span>
                     <input type="number" id="quantity" name="quantity" value="1" class=" border-2 rounded w-[80px] h-[30px] ">
-                    <button class="w-[60px] h-[30px] bg-stone-500 text-white rounded ">ADD TO CART</button>
+                    <button class="w-[60px] h-[30px] bg-stone-500 text-white rounded pt-1 ml-2 hover:bg-amber-600">ADD TO CART</button>
                 </span>
 
             </div>
         </div>
-        <p class="text-slate-500 mt-4 mb-6 pb-6 border-b border-slate-200 font-sans text-black">
+        <p class="text-slate-500 mt-4 mb-6 pb-6 border-b border-slate-200 font-sans text-black pt-4 leading-[25px]">
             <?php echo $product['detail'] ?>
         </p>
         <div class="flex space-x-4 mb-5 text-sm font-medium">
@@ -49,20 +49,20 @@
                 </svg>
             </button>
         </div>
-        <div class="h-[60px] pt-4 border-b border-t  " >
-        <scan class="font-sans text-base" >CATEGORIES:</scan>
-        <scan class="font-sans text-base">Son Mac Chili, Phấn Phủ MAC NC15</scan>
-        
+        <div class="h-[60px] pt-4 border-b border-t  ">
+            <scan class="font-sans text-base">CATEGORIES:</scan>
+            <scan class="font-sans text-base">Son Mac Chili, Phấn Phủ MAC NC15</scan>
+
         </div>
         <div class="pt-4">
             <scan class="font-sans"> SHARE THIS PRODUCT: </scan>
-          <scan class="pr-2"><i class="fa fa-facebook" aria-hidden="true"></i></scan>
-          <scan class="pr-2"><i class="fa fa-instagram" aria-hidden="true"></i></scan>
-          <san class="pr-2"><i class="fa fa-twitter" aria-hidden="true"></i></san>
-          <scan class="pr-2"><i class="fa fa-google-plus" aria-hidden="true"></i></scan>
+            <scan class="pr-2 hover:text-amber-600"><i class="fa fa-facebook" aria-hidden="true  "></i></scan>
+            <scan class="pr-2 hover:text-amber-600"><i class="fa fa-instagram" aria-hidden="true"></i></scan>
+            <san class="pr-2 hover:text-amber-600"><i class="fa fa-twitter" aria-hidden="true"></i></san>
+            <scan class="pr-2 hover:text-amber-600"><i class="fa fa-google-plus" aria-hidden="true"></i></scan>
         </div>
-       
-        
+
+
     </form>
 
 </div>
