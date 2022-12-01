05bb94f45063b1ca11a8b8eb3b207b797e87ffce
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-12-01 09:41:56 +0700

    up

--- modules/client/cart/controllers/indexController.php
+++ modules/client/cart/controllers/indexController.php
@@ -4,24 +4,28 @@ function construct()
     load_model('index');
     load('helper', 'format');
 }
+
 function indexAction()
 {
-
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
-        $pro = get_pro_by_id($id);
-        $qty = 1;
+        $product = get_product_by_id($id);
+        $quantity = 1;
         if (isset($_SESSION['cart']['buy']) && array_key_exists($id, $_SESSION['cart']['buy'])) {
-            $qty = $_SESSION['cart']['buy'][$id]['qty'] + 1;
+            $quantity = $_SESSION['cart']['buy'][$id]['quantity'] + 1;
         }
+
         $_SESSION['cart']['buy'][$id] = array(
-            'id' => $pro['id'],
-            'title' => $pro['title'],
-            'price' => $pro['price'],
-            'qty' => $qty,
-            'sub_total' => $pro['price'] * $qty
+            'id_product' => $product['id'],
+            'name_product' => $product['name_product'],
+            'image' => $product['image'],
+            'price' => $product['price'],
+            'quantity' => $quantity,
+            'sub_total' => $product['price'] * $quantity
         );
+
         update_info_cart();
+        header('Location: ?role=client&mod=cart');
     }
     if (!empty($_SESSION['cart'])) {
         $data['cart'] = $_SESSION['cart'];
@@ -39,12 +43,12 @@ function deleteAction()
             update_info_cart();
         }
     }
-    header('Location:?mod=cart');
+    header('Location: ?role=client&mod=cart');
 }
+
 function indexPostAction()
 {
-    show_array($_POST['qty']);
-    die;
-    update_cart($_POST['qty']);
-    header("Location:?mod=cart");
+    $quantity = $_POST['quantity'];
+    update_cart($quantity);
+    header("Location: ?role=client&mod=cart");
 }
--- modules/client/cart/models/indexModel.php
+++ modules/client/cart/models/indexModel.php
@@ -1,9 +1,10 @@
 <?php
-function get_pro_by_id($id)
+function get_product_by_id($id)
 {
-   $result = db_fetch_row("SELECT * FROM `products` WHERE id={$id}");
+   $result = db_fetch_row("SELECT * FROM `products` WHERE id = $id");
    return $result;
 }
+
 function get_total_cart()
 {
    if (isset($_SESSION['cart'])) {
@@ -11,12 +12,13 @@ function get_total_cart()
    }
    return FALSE;
 }
+
 function update_info_cart()
 {
    $num_order = 0;
    $total = 0;
    foreach ($_SESSION['cart']['buy'] as $item) {
-      $num_order += $item['qty'];
+      $num_order += $item['quantity'];
       $total += $item['sub_total'];
    }
    $_SESSION['cart']['infor'] = array(
@@ -24,11 +26,12 @@ function update_info_cart()
       'total' => $total,
    );
 }
-function update_cart($qty)
+
+function update_cart($quantity)
 {
-   foreach ($qty as $id => $new_qty) {
-      $qty = $_SESSION['cart']['buy'][$id]['qty'] = $new_qty;
-      $_SESSION['cart']['buy'][$id]['sub_total'] = $new_qty * $_SESSION['cart']['buy'][$id]['price'];
+   foreach ($quantity as $id => $new_quantity) {
+      $quantity = $_SESSION['cart']['buy'][$id]['quantity'] = $new_quantity;
+      $_SESSION['cart']['buy'][$id]['sub_total'] = $new_quantity * $_SESSION['cart']['buy'][$id]['price'];
    }
    update_info_cart();
 }
