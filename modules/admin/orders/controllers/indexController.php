eafaca8b97ef82fa149458d6ccd201cbffd8cdbf
luongtvph23841 <luongtvph23841@fpt.edu.vn>
2022-12-06 00:15:13 +0700

    up

--- modules/admin/orders/controllers/indexController.php
+++ modules/admin/orders/controllers/indexController.php
@@ -7,7 +7,22 @@ function construct()
 
 function indexAction()
 {
-
     $data['orders'] = get_list_orders();
     load_view('index', $data);
+}
+
+function updateAction()
+{
+    $status = get_list_status();
+    $data['status'] = $status;
+    load_view('update', $data);
+}
+
+function updatePostAction()
+{
+    $id_order = $_GET['id_order'];
+    $id_status = $_POST['id_status'];
+    update_order($id_order, $id_status);
+    push_notification('success', ['Chỉnh sửa đơn hàng thành công']);
+    header('Location: ?role=admin&mod=orders');
 }
--- modules/admin/orders/models/indexModel.php
+++ modules/admin/orders/models/indexModel.php
@@ -4,7 +4,19 @@ function get_list_orders()
     $result = db_fetch_array(" SELECT o.*, name_status, name_user FROM `orders` o 
     INNER JOIN `status` s ON s.id = o.id_status
     INNER JOIN `users` u ON u.id = o.id_user");
-
     return $result;
+}
 
+function get_list_status()
+{
+    $result = db_fetch_array(" SELECT * FROM `status`");
+    return $result;
+}
+
+function update_order($id, $status)
+{
+    db_update('orders', [
+        'id_status' => $status,
+    ], "id = $id");
+    return true;
 }
